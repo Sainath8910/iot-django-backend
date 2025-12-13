d22a3eb62bff7984ca600bdc9fc4f24f103c943c
sainath <nmachire@gitam.in>
2025-12-14 02:13:46 +0530

    hosts

--- iot_backend/settings.py
+++ iot_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c=5o1^s59ur&()e+14d*0c#@!o7)vbus491&ga8!kegpl1f2^*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
