6818bf5fa6599611c9c41ad0590391ac162fc227
sainath <nmachire@gitam.in>
2025-12-14 18:26:17 +0530

    ux

--- sensors/urls.py
+++ sensors/urls.py
@@ -3,6 +3,6 @@ from .views import sensor_data_api,dashboard,latest_readings
 
 urlpatterns = [
     path('sensor-data/', sensor_data_api),
-    path('dashboard/', dashboard),
+    path('', dashboard),
     path('latest/', latest_readings),
 ]
